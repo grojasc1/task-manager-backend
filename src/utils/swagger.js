11a625ab171fad44ac0126887b3bb80f254c81d4
Germán Rojas <g.rojasc@uniandes.edu.co>
2024-12-28 18:16:15 -0500

    update Swagger configuration for Task API to use OpenAPI 3.0 and improve server URLs

--- src/utils/swagger.js
+++ src/utils/swagger.js
@@ -2,15 +2,23 @@ const swaggerJSDoc = require('swagger-jsdoc');
 
 const swaggerOptions = {
     definition: {
+        openapi: '3.0.0',
         info: {
-            title: 'Task Manager API',
+            title: 'Task API',
             version: '1.0.0',
-            description: 'API for managing tasks',
+            description: 'A simple Express API for managing tasks',
         },
+        servers: [
+            {
+                url: process.env.NODE_ENV === 'production'
+                    ? 'https://task-manager-backend-production-5cb8.up.railway.app'  // URL de producción
+                    : 'http://localhost:5000',  // URL local para desarrollo
+            },
+        ],
     },
     apis: ['./src/routes/*.js'],
 };
 
-const swaggerDocs = swaggerJSDoc(swaggerOptions);
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+module.exports = swaggerSpec;
