0452e181a1d769b6bd46811e0dc504942119c76a
Germán Rojas <g.rojasc@uniandes.edu.co>
2024-12-29 15:48:46 -0500

    remove duplicate cors middleware usage in app.js

--- src/app.js
+++ src/app.js
@@ -12,11 +12,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
 app.use('/api/tasks', taskRoutes);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-app.use(cors());
 
 console.log('Documentations: http://localhost:5000/api-docs');
 
